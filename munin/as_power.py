fe5eb96ff0b038baa92ba4f1814af93b8237140c
Adrian Wiedemann <github-720656@kyubu.de>
2022-10-08 19:15:08 +0200

    Add energy and power munin plugins

---
+++ munin/as_power.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+import fritzconnection
+from fritzconnection.lib.fritzhomeauto import FritzHomeAutomation
+
+hostname = os.environ['hostname']
+login = os.environ['auth_username']
+password = os.environ['auth_password']
+munin_category = "energy"
+if 'category' in os.environ.keys():
+    munin_category = os.environ['category']
+
+fc = fritzconnection.FritzConnection(hostname, user=login, password=password)
+fha = FritzHomeAutomation(fc=fc)
+
+config = len(sys.argv) > 1 and sys.argv[1] == "config"
+
+if config:
+    sys.stdout.write("graph_title Current power consumption\n")
+    sys.stdout.write("graph_vlabel Power (Watts)\n")
+    sys.stdout.write(f"graph_category {munin_category}\n")
+    
+for entry in fha.device_information():
+    if entry['NewTemperatureIsEnabled'] != "ENABLED":
+        continue
+    ain = entry['NewAIN']
+    product_name = entry['NewProductName']
+    descr = entry['NewDeviceName']
+    power = "U"
+    if entry['NewMultimeterIsValid'] == "VALID":
+        power = int(entry['NewMultimeterPower']) / 100.0
+    ain_munin = "s" + ain.replace(" ", "")  # munin needs the data source start with a character
+
+    if config:
+        sys.stdout.write(f"{ain_munin}.label: {descr}\n")
+        continue
+
+    sys.stdout.write(f"{ain_munin}.value: {power}\n")
